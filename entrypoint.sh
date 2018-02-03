9fe4565498a494a43dba37f97da61541f8710b01
Mikael Kermorgant <mikael.kermorgant@gmail.com>
2018-02-03 12:15:36 +0200

    fixup! fixup! first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,6 @@
 # To run tests you need to pass some parameters to Behat container.
 # E.x., docker-compose exec behat /srv/entrypoint.sh "--format=pretty --out=std".
 
-# Pause until Composer update will be finished.
-if [ "$COMPOSER_UPDATE" -eq "1" ]; then
-    while [  ! -e "/tmp/update_finished" ]; do
-        sleep 1
-    done
-fi
-
 # Run Behat with parameters passed as command.
 bin/behat $*
 
